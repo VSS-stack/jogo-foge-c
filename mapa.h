2f5a8ef9ca1fc754c2ecc801d259a22933d4c0e0
VSS-stack <victor.stsilva93@gmail.com>
2023-06-11 14:23:18 -0300

    melhorando função para encontrar personagem no jogo

--- mapa.h
+++ mapa.h
@@ -4,7 +4,13 @@ typedef struct mapa {
     int colunas;
 } MAPA;
 
+typedef struct posicao {
+    int x;
+    int y;
+} POSICAO;
+
 void liberamapa(MAPA *m);
 void lemapa(MAPA *m);
 void alocamapa(MAPA *m);
 void imprimemapa(MAPA *m);
+void encontranomapa(MAPA *m, POSICAO *p, char c);
