8e4d25576a2c08fb7f0d51fc9210f57641c68d31
VSS-stack <victor.stsilva93@gmail.com>
2023-07-04 20:51:31 -0300

    melhorando funções de movimentação

--- mapa.h
+++ mapa.h
@@ -24,4 +24,6 @@ int ehvalida(MAPA *m, int x, int y);
 int ehvazia(MAPA *m, int x, int y);
 void movepersonagem(MAPA *m, int xorigem, int yorigem, int xdestino, int ydestino);
 void copiamapa(MAPA *destino, MAPA *origem);
-int podeandar(MAPA *m, int x, int y);
+int podeandar(MAPA *m, char personagem, int x, int y);
+int ehparede(MAPA *m, int x, int y);
+int ehpersonagem(MAPA *m, char personagem, int x, int y);
